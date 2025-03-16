407d90a1a84a64e8237d9d7a5d12b25884a68616
sebamiro <seba.mi.21@gmail.com>
2025-03-16 18:27:24 +0100

    todo: arana

--- arena.c
+++ arena.c
@@ -1,9 +1,44 @@
+#include <unistd.h>
+#include <assert.h>
+#include <sys/mmap.h>
+
+/*
+ * region: linked list of the actual allocation buffers; when an allocation is requested
+ * a region able to fit the requested size is looked for. If none is found a new
+ * region is added to the list.
+ */
+typedef struct region;
+struct region
+{
+	region*		next;
+	u64			cur;
+	u64			size;
+	uintpr_t	data[];
+};
+
 typedef struct arena
 {
-	u8*	buf;
-	u64 cur;
-	u64 len;
+	region* begin;
+	region* end;
 } arena;
+
+typedef struct arena_mark
+{
+	region* region;
+	u64		cur;
+} arena;
+
+region* newRegion(u64 size)
+{
+	region* res;
+	u64 sizeMap = sizeof(region) + sizeof(u8)*size;
+	res = mmap(NULL, sizeMap, PROT_READ|PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
+	assert(res != MAP_ERROR);
+	res.next = NULL;
+	res.cur = 0;
+	res.size = size;
+	return res;
+}
 
 arena Arena_Init(u64 len)
 {
